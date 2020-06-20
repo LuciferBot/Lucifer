5894203033fc5adcacc212036a573780a8ae0071
thakorrohan22 <48714583+thakorrohan22@users.noreply.github.com>
2020-06-20 10:24:27 +0000

    Add files via upload

--- lucifer/__main__.py
+++ lucifer/__main__.py
@@ -48,7 +48,7 @@ GDPR = []
 
 
 for module_name in ALL_MODULES:
-    imported_module = importlib.import_module("spongebob.modules." + module_name)
+    imported_module = importlib.import_module("lucifer.modules." + module_name)
     if not hasattr(imported_module, "__mod_name__"):
         imported_module.__mod_name__ = imported_module.__name__
 
